7aa2087f8398f5fa8f8a1116e128ad528228ddad
codecB <codecB@abc.def>
2015-11-11 13:16:28 -0500

    initial commit

---
+++ src/main/java/com/newsoft/hello/App.java
@@ -0,0 +1,13 @@
+package com.newsoft.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
