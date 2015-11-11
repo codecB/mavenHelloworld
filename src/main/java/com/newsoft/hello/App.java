e987ce6a4b799867c1cf82958f9bc8fc07cfdb5b
codecB <codecB@abc.def>
2015-11-11 13:35:05 -0500

    added dependency to joda time library;

--- src/main/java/com/newsoft/hello/App.java
+++ src/main/java/com/newsoft/hello/App.java
@@ -1,13 +1,17 @@
 package com.newsoft.hello;
 
+import org.joda.time.DateTime;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        DateTime dt = new DateTime();
+        int year = dt.getYear();
+
+        System.out.println( "Hello World from "+year);
     }
 }
