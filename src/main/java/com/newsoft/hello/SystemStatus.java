9285e6dd6443d4348cdf11d6fa874b0675e4fd7c
Yang Bo <yang.bo@gses.com>
2015-11-12 18:14:23 -0500

    display memory in GB

--- src/main/java/com/newsoft/hello/SystemStatus.java
+++ src/main/java/com/newsoft/hello/SystemStatus.java
@@ -4,6 +4,10 @@ import org.hyperic.sigar.Mem;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
 
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class SystemStatus {
 
     private static Sigar sigar = new Sigar();
@@ -20,18 +24,34 @@ public class SystemStatus {
             se.printStackTrace();
         }
 
+//        System.out.println("Actual total free system memory: "
+//                + mem.getActualFree() / 1024 / 1024+ " MB");
+//        System.out.println("Actual total used system memory: "
+//                + mem.getActualUsed() / 1024 / 1024 + " MB");
+//        System.out.println("Total free system memory ......: " + mem.getFree()
+//                / 1024 / 1024+ " MB");
+//        System.out.println("System Random Access Memory....: " + mem.getRam()
+//                + " MB");
+//        System.out.println("Total system memory............: " + mem.getTotal()
+//                / 1024 / 1024+ " MB");
+//        System.out.println("Total used system memory.......: " + mem.getUsed()
+//                / 1024 / 1024+ " MB");
+
+
+        NumberFormat formatter = new DecimalFormat("#0.000");
+
         System.out.println("Actual total free system memory: "
-                + mem.getActualFree() / 1024 / 1024+ " MB");
+                + formatter.format((double)mem.getActualFree() / 1024 / 1024 /1024)+ " GB");
         System.out.println("Actual total used system memory: "
-                + mem.getActualUsed() / 1024 / 1024 + " MB");
-        System.out.println("Total free system memory ......: " + mem.getFree()
-                / 1024 / 1024+ " MB");
-        System.out.println("System Random Access Memory....: " + mem.getRam()
-                + " MB");
-        System.out.println("Total system memory............: " + mem.getTotal()
-                / 1024 / 1024+ " MB");
-        System.out.println("Total used system memory.......: " + mem.getUsed()
-                / 1024 / 1024+ " MB");
+                + formatter.format((double)mem.getActualUsed() / 1024 / 1024/1024) + " GB");
+        System.out.println("Total free system memory ......: " + formatter.format((double)mem.getFree()
+                / 1024 / 1024/1024)+ " GB");
+        System.out.println("System Random Access Memory....: " + formatter.format((double)mem.getRam()/1024)
+                + " GB");
+        System.out.println("Total system memory............: " + formatter.format((double)mem.getTotal()
+                / 1024 / 1024/1024)+ " GB");
+        System.out.println("Total used system memory.......: " + formatter.format((double)mem.getUsed()
+                / 1024 / 1024/1024)+ " GB");
 
         System.out.println("\n**************************************\n");
     }
