ef2789b3a7861001e2546f4bb72e6adea31bf24d
Yang Bo <yang.bo@gses.com>
2015-11-12 13:36:33 -0500

    display memory status

--- src/main/java/com/newsoft/hello/App.java
+++ src/main/java/com/newsoft/hello/App.java
@@ -1,6 +1,8 @@
 package com.newsoft.hello;
 
 import org.joda.time.DateTime;
+import java.lang.reflect.Field;
+
 
 /**
  * Hello world!
@@ -11,7 +13,26 @@ public class App
     public static void main( String[] args ) {
         DateTime dt = new DateTime();
         int year = dt.getYear();
+        System.out.println( "Hello from "+year);
 
-        System.out.println( "Hello World from "+year);
+        SystemStatus.getMemoryUsage();
+    }
+
+    /**
+     * Sets JVM argument java.library.path to use the dll.
+     * Not a clean solution but it works.
+     */
+    private static void setLibraryPath(){
+        System.setProperty( "java.library.path",  System.getProperty("user.dir") + "\\lib" );
+        Field fieldSysPath = null;
+        try {
+            fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
+            fieldSysPath.setAccessible( true );
+            fieldSysPath.set( null, null );
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
